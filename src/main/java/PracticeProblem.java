cac6b8cd6a891df1cabb7dbbff6faeba2bcdb0cf
VisaaganG <343505582@tdsb.ca>
2026-02-24 16:36:51 +0000

    Questions Finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,10 +5,10 @@
  * Date Last Edited: March 29, 2019
  */
 
-
+import java.util.Scanner;
 class PracticeProblem {
 
-	static Scanner s = new Scanner();
+	Scanner s = new Scanner(System.in);
   	public static void main(String[] args) {
 		
 		q1();
@@ -18,41 +18,44 @@ class PracticeProblem {
   	}
   	public static void q1() {
 		// From input, recieve two integers from the user and add them together.  Output the result.
-
-		System.out.println("Input a number: ");
-		num1 = s.nextInt;
-		System.out.println("Input a number: ");
-		num2 = s.nextInt;
-
-		System.out.println(num + num2);
+		Scanner s = new Scanner(System.in);
+		int num1;
+		int num2;
+		System.out.print("Input a number: ");
+		num1 = s.nextInt();
+		System.out.print("Input a number: ");
+		num2 = s.nextInt();
+		System.out.println(num1 + num2);
   	}
 
 	public static void q2() {
 		// From input recieve two integers.  Output the quotient rounded down.
-		
-		System.out.println("Input a number: ");
-		num1 = s.nextLine();
-		System.out.println("Input a number: ");
-		num2 = s.nextLine();
-		
-		system.out.println(num1/num2);
+		Scanner s = new Scanner(System.in);
+		int num1;
+		int num2;
+		System.out.print("Input a number: ");
+		num1 = s.nextInt();
+		System.out.print("Input a number: ");
+		num2 = s.nextInt();
+		System.out.println(num1/num2);
 	}
 
 	public static void q3() {
-		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?"
-		
-		System.out.println(hello "Mr. Kalisz" have you seen my work yet?);
+		// Output the phrase "hello "Mr. Kalisz" have you seen my work yet?" 
+		Scanner s = new Scanner(System.in);
+		System.out.println("hello \"Mr. Kalisz\" have you seen my work yet?");
 	}
 
 	public static void q4() {
 		// From input recieve two numbers (can be decimal fractions).  
 		// Output the first number plus two, then multiplied by the second number.
-		
-		System.out.println("Input a number: ");
-		s.nextInt();
-		System.out.println("Input a number: ");
-		num2 = s.nextInt();
-		
-		System.out.println(num1 + 2 * num2)
+		Scanner s = new Scanner(System.in);
+		double num2;
+		double num1;
+		System.out.print("Input a number: ");
+		num1 = s.nextDouble();
+		System.out.print("Input a number: ");
+		num2 = s.nextDouble();
+		System.out.println((num1 + 2) * num2);
 	}
 }
